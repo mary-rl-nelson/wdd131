d6dfd985a994c1f2ea20292b6b3c2896d66b98b9
Mary Frazier <161389971+mary-rl-nelson@users.noreply.github.com>
2026-09-20 16:18:27 -0500

    added another area

--- ponder/variables-constants.js
+++ ponder/variables-constants.js
@@ -3,6 +3,11 @@ let radius = 3;
 
 let area = PI * radius * radius;
 
-console.log(area)
+console.log(area);
 // console.log is used to print the value of area to the console. The area of a circle is calculated using the formula area = π * r^2, where r is the radius of the circle. In this case, the radius is 3, so the area is calculated as 3.14 * 3 * 3, which equals 28.26.
 
+radius = 20;
+
+area = PI * radius * radius;
+
+console.log(area);
